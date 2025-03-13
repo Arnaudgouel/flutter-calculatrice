fa584857ded42ca98204a7cd70154ce8ba2edf7e
Arnaud Gouel <gouel.arnaud@gmail.com>
2025-03-13 18:10:29 +0100

    explication de mes choix et problèmes rencontrés

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ma Calculatrice',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,97 +30,31 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: const CalculatorScreen(),
     );
   }
 }
 
-/*class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}*/
+/*
+* Chaque bouton appelle _onButtonPressed(value), qui met à jour l’état de l’application via setState().
+* Une variable _firstOperand stocke le premier nombre entré.
+* Une variable _operator stocke l’opérateur sélectionné.
+* Lorsqu’un utilisateur appuie sur "=", le deuxième nombre est récupéré et le calcul est effectué.
+*
+* Mes difficultés que j'ai rencontrées
+*
+* Gestion des entrées utilisateur. Problème : L’utilisateur pouvait entrer plusieurs zéros en début de saisie, ou concaténer les opérations sans validation.
+* Solution : Ajout d’une variable _isNewInput pour effacer l’affichage lors d’une nouvelle entrée.
+*
+* Gestion des erreurs mathématiques. Problème : Division par zéro entraînait un plantage de l’application.
+* Solution : Ajout d’un test if (secondOperand != 0) pour afficher "Erreur" en cas de division par zéro.
+*
+* Affichage de nombres à virgule flottante. Problème : Le résultat affichait des nombres avec trop de décimales (ex: 2.0000000001).
+* Solution : Arrondi automatique via .toStringAsFixed(2) (option que l'on pourrait ajouter pour améliorer la lisibilité).
+*
+* J'ai encore le problème d'affichage du resultat avec une virgule puis un zero même si le nombre est un entier.
+* Je m'arrête sur ça pour ne pas passer encore trop de temps sur l'application
+*/
 
 class CalculatorScreen extends StatefulWidget {
 
